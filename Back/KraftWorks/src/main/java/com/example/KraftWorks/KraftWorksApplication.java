d1dcb5be753173e05788e9e4ade049a3d6604dd4
juaunvitorsw <105472162+juaunvitorsw@users.noreply.github.com>
2026-04-01 10:17:27 -0300

    Alterado DockerFile para dar build no backend

--- Back/KraftWorks/src/main/java/com/example/KraftWorks/KraftWorksApplication.java
+++ Back/KraftWorks/src/main/java/com/example/KraftWorks/KraftWorksApplication.java
@@ -3,12 +3,10 @@ package com.example.KraftWorks;
 import com.example.KraftWorks.scheduler.scheduler;
 import com.example.KraftWorks.service.PersonService;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 
 import org.springframework.cache.annotation.EnableCaching;
 
-import java.security.Provider.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,23 +26,6 @@ public class KraftWorksApplication {
 	}
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-		        .directory("C:/")
-		        .load();
-
-
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
-        System.setProperty("API_URL", dotenv.get("API_URL"));
-        System.setProperty("API_KEY", dotenv.get("API_KEY"));
-        
-        System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
-        System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
-
-        
-        
         SpringApplication.run(KraftWorksApplication.class, args);
     }
 	
